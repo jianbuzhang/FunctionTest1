862f81d2b05acf371298b1dc56b4033225d5722c
shuimanting <1528038517@qq.com>
2017-12-06 21:12:53 +0800

    完善主测试程序MultiFunctionTest

---
+++ MultiFunctionTest/MultiFunctionTest/MultiFunctionTest/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MultiFunctionTest.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDP_SOCKETS_INIT_FAILED         104
+#define IDR_MAINFRAME                   128
+#define IDR_MultiFunctionTeTYPE         130
+#define ID_WINDOW_MANAGER               131
+#define IDD_BASICSETTING                310
+#define IDC_CHECK_ACCETEST1             1000
+#define ID_EDIT_TESTACCELERATION        32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif

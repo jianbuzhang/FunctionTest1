df0f1e256e1788efc79db641fa4e2a620155c3cb
shuimanting <1528038517@qq.com>
2017-12-06 19:47:15 +0800

    3完成加速度传感器测试程序初稿

---
+++ AccelerationTest/AccelerationTest/AccelerationTest/AccelerationTest.cpp
@@ -0,0 +1,28 @@
+// AccelerationTest.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "AccelerationTest.h"
+#include <iostream>
+
+using namespace std;
+
+int InitAcceTest() 
+{
+	cout<<"初始化传感器测试"<<endl;
+	return 0;
+}
+
+int SendComToDUT() 
+{
+	cout<<"传感器测试：发送指令到DUT"<<endl;
+	return 0;
+}
+
+int ReceiveCmdFromDUT() 
+{
+	cout<<"传感器测试：接收来自DUT的指令"<<endl;
+	return 0;
+}
+
+
---
+++ AccelerationTest/AccelerationTest/AccelerationTest/AccelerationTest.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//http://blog.csdn.net/m0_37170593/article/details/76445972
+
+__declspec(dllexport) int InitAcceTest();
+__declspec(dllexport) int SendComToDUT();
+__declspec(dllexport) int ReceiveCmdFromDUT();
+

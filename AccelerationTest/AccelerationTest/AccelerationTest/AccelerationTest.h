c6cf7587a3a98acb80959029ae5c744b88004c0c
shuimanting <1528038517@qq.com>
2017-12-12 22:50:54 +0800

    1212下班前

--- AccelerationTest/AccelerationTest/AccelerationTest/AccelerationTest.h
+++ AccelerationTest/AccelerationTest/AccelerationTest/AccelerationTest.h
@@ -2,10 +2,13 @@
 //²Î¿¼²©¿Í
 //http://blog.csdn.net/m0_37170593/article/details/76445972
 
+//add dll lib for the function
 //__declspec(dllexport) int InitAcceTest();
 //__declspec(dllexport) int SendComToDUT();
 //__declspec(dllexport) int ReceiveCmdFromDUT();
 
+
+//add dll lib for the class
 class  _declspec(dllexport) AccelerationTest
 {
 public:
